62cd0130a0af7306c27f9b835854e63ae69c57fa
Juan Ramos <juan2ramos@gmail.com>
2015-08-13 19:55:08 -0500

    first commit

---
+++ application/views/form/index.php
@@ -0,0 +1,94 @@
+<main>
+    <h1>REGISTRO RED BULL RACING CAN</h1>
+
+    <p>Regístrate en RedBull.com, dibuja y muéstranos el diseño de tu Racing Can de un auto a control remoto con
+        carrocería hecha de latas de Red Bull, los 10 mejores diseños por ciudad serán elegidos por un jurado para
+        recibir el chasis del auto y un kit para participar en los calificatorios para la final nacional.</p>
+
+    <form action="">
+
+        <div class="input-content">
+            <label for="team">Nombre del Equipo: </label>
+            <input type="text" name="team" >
+        </div>
+        <div class="input-content">
+            <label for="city">Ciudad: </label>
+            <select name="city" id="">
+                <option value="Monterrey">Monterrey</option>
+                <option value="Tijuana">Tijuana</option>
+                <option value="Guadalajara">Guadalajara</option>
+                <option value="Puebla">Puebla</option>
+                <option value="D.F.">D.F.</option>
+            </select>
+        </div>
+
+        <fieldset>
+            <legend>Miembro 1 del equipo</legend>
+            <div class="input-content">
+                <label for="team">Nombre: </label>
+                <input type="text" name="name-user" >
+            </div>
+            <div class="input-content">
+                <label for="team">Apellido: </label>
+                <input type="text" name="last-name" >
+            </div>
+            <div class="input-content">
+                <label for="team">Fecha de nacimiento: </label>
+                <input type="date" name="birthday" >
+            </div>
+            <div class="input-content">
+                <label for="team">E-mail: </label>
+                <input type="text" name="email" >
+            </div><div class="input-content">
+                <label for="team">Teléfono: </label>
+                <input type="text" name="phone" >
+            </div>
+            <div class="input-content">
+                <label for="team">Dirección: </label>
+                <input type="text" name="address" >
+            </div>
+            <div class="input-content">
+                <label for="team">Número de estudiante universitario: </label>
+                <input type="text" name="team" >
+            </div>
+
+
+        </fieldset>
+
+        <fieldset>
+            <legend>Miembro 2 del equipo</legend>
+            <div class="input-content">
+                <label for="team">Nombre: </label>
+                <input type="text" name="name-user" >
+            </div>
+            <div class="input-content">
+                <label for="team">Apellido: </label>
+                <input type="text" name="last-name" >
+            </div>
+            <div class="input-content">
+                <label for="team">Fecha de nacimiento: </label>
+                <input type="date" name="birthday" >
+            </div>
+            <div class="input-content">
+                <label for="team">E-mail: </label>
+                <input type="text" name="email" >
+            </div><div class="input-content">
+                <label for="team">Teléfono: </label>
+                <input type="text" name="phone" >
+            </div>
+            <div class="input-content">
+                <label for="team">Dirección: </label>
+                <input type="text" name="address" >
+            </div>
+            <div class="input-content">
+                <label for="team">Número de estudiante universitario: </label>
+                <input type="text" name="team" >
+            </div>
+
+
+        </fieldset>
+        <input type="file">
+        <button> GUARDAR </button>
+    </form>
+
+</main>
